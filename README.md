81607f9f3cf0af65f16d0cb2fc3057cfbf753b31
humam-hossain <humam.hossain.e@gmail.com>
2022-12-13 21:01:37 +0600

    added: edge cases

--- README.md
+++ README.md
@@ -9,9 +9,18 @@ $ python main.py
 ```
 
 ## Sample Output
-![Output Sample](https://i.ibb.co/R4vx9Cp/output-tshirt-sample-2.png)
-![Output Sample](https://i.ibb.co/qg1K4wF/output-tshirt-sample-1.png)
-![Output Sample](https://i.ibb.co/8zpVbZM/output-sample.png)
+![Output Sample](https://i.ibb.co/kgb6J90/output-tshirt-sample-2.png)
+![Output Sample](https://i.ibb.co/48fsp74/output-tshirt-sample-1.png)
+![Output Sample](https://i.ibb.co/C8wBj1j/output-sample.png)
+
+## Limitations
 
 ### Noise
-![Output Noise](https://i.ibb.co/4jXSnzN/output-noise.png)
+Noise exists in output
+
+![Output Noise](https://i.ibb.co/7KPFjmN/output-noise.png)
+
+### Complex sample
+Doesn't work on complex samples
+
+![Output complex sample](https://i.ibb.co/stvwZ25/output-complex.png)
--- src/crop.py
+++ src/crop.py
@@ -1,16 +1,7 @@
 import cv2 as cv
 
-def rescaleImg(img, scale=0.75):
-    height = int(img.shape[0] * scale)
-    width = int(img.shape[1] * scale)
-
-    dimensions = (width, height)
-
-    return cv.resize(img, dimensions, interpolation=cv.INTER_AREA)
-
-ref = cv.imread("../photos/ref.jpg")
-ref2 = cv.imread("../photos/ref2.jpg")
-
+ref = cv.imread("../photos/complex_original.jpg")
+ref2 = cv.imread("../photos/comlex_sample.jpg")
 
 ref = ref[1000:-1000, 500:-500]
 ref2 = ref2[1000:-1000, 500:-500]
@@ -18,12 +9,8 @@ ref2 = ref2[1000:-1000, 500:-500]
 print(ref.shape)
 print(ref2.shape)
 
-cv.imwrite("ref1.jpg", ref)
-cv.imwrite("ref2.jpg", ref2)
-
-scale = 0.2
-cv.imshow("ref", rescaleImg(ref, scale))
-cv.imshow("ref2", rescaleImg(ref2, scale))
+cv.imwrite("complex_original.jpg", ref)
+cv.imwrite("complex_sample.jpg", ref2)
 
 
 cv.waitKey(0)
